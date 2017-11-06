f47d7893d862bce301ff6db6ae6599dce1bd1e7a
Jacek Bajor <jacek.m.bajor@vanderbilt.edu>
2017-11-06 14:41:10 -0600

    Script to generate diabetes documents

---
+++ diabetes_dataset_generation/6_build_documents.py
@@ -0,0 +1,19 @@
+import dill
+import pandas as pd
+from tqdm import tqdm
+
+PATIENT_HISTORY_FILE = '../data/raw/diabetes_patients_codes.csv'
+DOCUMENTS_OUTPUT_FILE = '../data/intermediate/diabetes_documents.dill'
+
+tp = pd.read_csv(PATIENT_HISTORY_FILE, iterator=True, chunksize=10**5, sep='|', index_col=None, header=None)
+patient_history_df = pd.concat(tp, ignore_index=True)
+patient_history_df.columns = ["IND_SEQ", "ENTRY_DATE", "CODE"]
+patient_history_groups_by_patient = patient_history_df.groupby(by=['IND_SEQ'])
+
+documents = {}
+
+for patient, patient_history in tqdm(patient_history_df.groupby(by=['IND_SEQ']), leave=True):
+    patient_codes = patient_history['CODE'].values.tolist()
+    documents[patient] = patient_codes
+
+dill.dump(documents, open(DOCUMENTS_OUTPUT_FILE, 'wb'))

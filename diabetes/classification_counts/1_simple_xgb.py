fb10c5f84c5177dcce64caaa84d6c61b65985b76
Jacek Bajor <jacek.m.bajor@vanderbilt.edu>
2017-12-04 10:55:04 -0600

    Added logs for diabetes classifiers

--- diabetes/classification_counts/1_simple_xgb.py
+++ diabetes/classification_counts/1_simple_xgb.py
@@ -8,7 +8,7 @@ from xgboost.sklearn import XGBClassifier
 from sklearn.metrics import log_loss, roc_auc_score
 
 DATA_FILE = '../data/final/counts/diabetes_counts.dill'
-LOG_FILE = '../../log/diabetes_counts_simple_xgb.log'
+LOG_FILE = '../log/diabetes_counts_simple_xgb.log'
 N_JOBS = 30
 N_ESTIMATORS = 2000
 
--- diabetes/classification_vectors/1_simple_xgb.py
+++ diabetes/classification_vectors/1_simple_xgb.py
@@ -8,7 +8,7 @@ from xgboost.sklearn import XGBClassifier
 from sklearn.metrics import log_loss, roc_auc_score
 
 DATA_DIR = '../data/final/vectors/'
-LOG_FILE = '../../log/diabetes_vectors_simple_xgb.log'
+LOG_FILE = '../log/diabetes_vectors_simple_xgb.log'
 N_JOBS = 35
 N_ESTIMATORS = 2000
 

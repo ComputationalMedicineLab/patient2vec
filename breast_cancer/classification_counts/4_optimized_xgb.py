af91b62f1d31e533bd06a04f758dc0e9375a668e
Jacek Bajor <jacek.m.bajor@vanderbilt.edu>
2017-12-14 08:34:52 -0600

    Breast cancer - added train and test optimized script

---
+++ breast_cancer/classification_counts/4_optimized_xgb.py
@@ -0,0 +1,52 @@
+import datetime
+import logging
+import os
+
+import dill
+import numpy as np
+from xgboost.sklearn import XGBClassifier
+from sklearn.metrics import log_loss, roc_auc_score
+
+DATA_FILE = '../data/final/counts/breast_cancer_counts.dill'
+LOG_FILE = '../log/breast_cancer_counts_optimized_xgb.log'
+N_JOBS = 30
+
+optimized_params = {
+    'colsample_bytree': 0.7,
+    'gamma': 0,
+    'learning_rate': 0.01,
+    'max_depth': 6,
+    'min_child_weight': 3,
+    'n_estimators': 3000,
+    'reg_alpha': 0.05,
+    'subsample': 0.7,
+    # other params:
+    'random_state': 1,
+    'silent': False,
+    'n_jobs': N_JOBS
+}
+
+# Logging setup
+logging.basicConfig(filename=LOG_FILE, level=logging.INFO,
+                    format='%(message)s')
+
+# Loading data
+data = dill.load(open(DATA_FILE, 'rb'))
+
+for months_before in sorted(list(data.keys())):
+    train_x = data[months_before]["TRAIN"]["X"]
+    train_y = data[months_before]["TRAIN"]["y"]
+    test_x = data[months_before]["TEST"]["X"]
+    test_y = data[months_before]["TEST"]["y"]
+
+    # Creating and training model
+    clf = XGBClassifier(**optimized_params)
+    clf.fit(train_x, train_y, verbose=True)
+
+    # Scoring
+    pred_y = clf.predict_proba(test_x)
+
+    auc_score = roc_auc_score(test_y, pred_y[:,1])
+    log_score = log_loss(test_y, pred_y)
+
+    logging.info('{}, {}, {}'.format(months_before, auc_score, log_score))

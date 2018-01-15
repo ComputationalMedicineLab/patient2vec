4058f3adba7d26a8fc2be1cccb9078360c878ed9
Jacek Bajor <jacek.m.bajor@vanderbilt.edu>
2018-01-15 10:57:54 -0600

    Diabetes - updated results, summary

--- diabetes/classification_counts/3_elastic_net.py
+++ diabetes/classification_counts/3_elastic_net.py
@@ -9,16 +9,19 @@ from sklearn.metrics import log_loss, roc_auc_score
 
 DATA_FILE = '../data/final/counts/diabetes_counts.dill'
 LOG_FILE = '../log/diabetes_counts_optimized_elasticnet.log'
+RESULTS_FILE = '../log/diabetes_counts_elasticnet_results.dill'
 
 optimized_params = {
     'alpha': 0.01,
-    'l1_ratio': 0.18099719057889352,
+    'l1_ratio': 0.014952629369956116,
     # other params:
     'loss': 'log',
     'penalty': 'elasticnet',
     'max_iter': 1000,
     'random_state': 1
 }
+
+results = {}
 
 # Logging setup
 logging.basicConfig(filename=LOG_FILE, level=logging.INFO,
@@ -43,4 +46,10 @@ for months_before in sorted(list(data.keys())):
     auc_score = roc_auc_score(test_y, pred_y[:,1])
     log_score = log_loss(test_y, pred_y)
 
+    results[months_before] = {}
+    results[months_before]['true_y'] = test_y
+    results[months_before]['pred_y'] = pred_y
+
     logging.info('{}, {}, {}'.format(months_before, auc_score, log_score))
+
+dill.dump(results, open(RESULTS_FILE, 'wb'))
